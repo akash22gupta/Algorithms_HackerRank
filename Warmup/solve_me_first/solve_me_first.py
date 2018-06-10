7caa9e1a3bb2278af4ceb0f9124636c7b5de1f92
Akash Gupta <akash@hawk.iit.edu>
2018-06-09 22:22:39 -0500

    seperate problem in separate folder

---
+++ Warmup/solve_me_first/solve_me_first.py
@@ -0,0 +1,10 @@
+def solveMeFirst(a,b):
+"""
+Returns the sum of a and b
+"""
+    return a+b # returns sum of a and b
+
+x = int(input()) # inputs value x
+y = int(input()) # inputs value y
+res = solveMeFirst(x,y) # calls function solveMeFirst and store the result in res
+print(res) # prints res
